4bb59918487b3d70563feb887a956458085cff01
trafficlight <wendyscashier1@gmail.com>
2024-08-30 22:01:49 +1000

    fix catppuccin

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #SDDM THEME
-sudo cp -r sddm/mcatppuccin-mocha /usr/share/sddm/themes/
+sudo cp -r sddm/catppuccin-mocha /usr/share/sddm/themes/
 sudo cp sddm/sddm.conf /etc/
 #dot files
 sudo cp -r dotfiles/.config/* /home/$USER/.config/
