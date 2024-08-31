5ce5ddfae238f25e4948b2546ca54b09143f05b3
thgilciffart <wendyscashier1@gmail.com>
2024-08-31 13:25:33 +1000

    spotify working

--- config.sh
+++ config.sh
@@ -3,3 +3,4 @@ sudo cp -r sddm/catppuccin-mocha /usr/share/sddm/themes/
 sudo cp sddm/sddm.conf /etc/
 #dot files
 sudo cp -r dotfiles/.config/* /home/$USER/.config/
+sudo cp dotfiles/com.spotify.Client.desktop ~/.local/share/applications/
--- flatpak.sh
+++ flatpak.sh
@@ -1,4 +1,6 @@
- flatpak install --user https://sober.vinegarhq.org/sober.flatpakref 
- flatpak install --user flathub dev.vencord.Vesktop
- flatpak install --user flathub io.missioncenter.MissionCenter
- flatpak install --user flathub io.github.flattool.Warehouse
+flatpak install --user flathub com.github.tchx84.Flatseal
+flatpak install --user https://sober.vinegarhq.org/sober.flatpakref 
+flatpak install --user flathub dev.vencord.Vesktop
+flatpak install --user flathub io.missioncenter.MissionCenter
+flatpak install --user flathub io.github.flattool.Warehouse
+flatpak install --user flathub com.spotify.Client
