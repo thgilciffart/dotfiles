e325de4c9bfdb74e2c205b2621e81f92edf25514
thgilciffart <wendyscashier1@gmail.com>
2024-09-01 21:12:58 +1000

    fix flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,6 +1,6 @@
-flatpak install --user flathub com.github.tchx84.Flatseal
-flatpak install --user https://sober.vinegarhq.org/sober.flatpakref 
-flatpak install --user flathub dev.vencord.Vesktop
-flatpak install --user flathub io.missioncenter.MissionCenter
-flatpak install --user flathub io.github.flattool.Warehouse
-flatpak install --user flathub com.spotify.Client
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub io.github.flattool.Warehouse
+flatpak install flathub io.missioncenter.MissionCenter
+flatpak install flathub dev.vencord.Vesktop
+flatpak install flathub com.spotify.Client
+flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
