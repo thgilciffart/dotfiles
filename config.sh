bcd50a81699ec9d5beef869d2327353aa3102195
thgilciffart <wendyscashier1@gmail.com>
2024-09-04 18:47:23 +1000

    sddm service enable

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #SDDM THEME
 sudo cp -r sddm/catppuccin-mocha /usr/share/sddm/themes/
 sudo cp sddm/sddm.conf /etc/
+systemctl enable sddm.service --force
 #dot files
 sudo cp -r dotfiles/.config/* /home/$USER/.config/
 
